25d7dfcaf449ab9d850ec91b36f5c6a9796f2dc9
victoraw97 <72541996+victoraw97@users.noreply.github.com>
2025-02-19 22:24:23 +0800

    Update 03. Data cleaning.sql

--- 03. Data cleaning.sql
+++ 03. Data cleaning.sql
@@ -1,20 +1,7 @@
--- create new clean table & new column (ride_length)
+-- create a new table for day of week, month and ride_length columns
 create table Cycalitic_Trips.clean_combined_table_2022 as
-  select * , (
-    extract(HOUR from (ended_at - started_at)) * 60 +
-    extract(MINUTE from (ended_at - started_at)) +
-    extract(SECOND from (ended_at - started_at)) / 60) as ride_length
-   from`lithe-bazaar-443112-i8.Cycalitic_Trips.combined_table_2022`
-
-  
--- check the new table does contain new column 
-select *
-   from`lithe-bazaar-443112-i8.Cycalitic_Trips.clean_combined_table_2022`
-
-
--- Find out how many ride_length > 1 day, and create the day of week and month columns 
 select *, 
-case extract (DAYOFWEEK from started_at)
+  case extract (DAYOFWEEK from started_at)
   when 1 then 'SUN'
   when 2 then 'MON'
   when 3 then 'TUE'
@@ -23,7 +10,7 @@ case extract (DAYOFWEEK from started_at)
   when 6 then 'FRI'
   when 7 then 'SAT'
   end as day_of_week,
-case extract (MONTH from started_at)
+    case extract (MONTH from started_at)
   when 1 then 'Jan'
   when 2 then 'Feb'
   when 3 then 'Mar'
@@ -36,7 +23,19 @@ case extract (MONTH from started_at)
   when 10 then 'Oct'
   when 11 then 'Nov'
   when 12 then 'Dec'
-  end as Month
+  end as Month,
+    (extract(HOUR from (ended_at - started_at)) * 60 +
+    extract(MINUTE from (ended_at - started_at)) +
+    extract(SECOND from (ended_at - started_at)) / 60) as ride_length
+   from`lithe-bazaar-443112-i8.Cycalitic_Trips.combined_table_2022`
+  
+-- check the new column in the new table 
+select *
+   from`lithe-bazaar-443112-i8.Cycalitic_Trips.clean_combined_table_2022`
+
+
+-- Find out how many ride_length > 1 day, and create the day of week and month columns 
+select *, 
    from`lithe-bazaar-443112-i8.Cycalitic_Trips.clean_combined_table_2022`
   where start_station_name is not null and 
   end_station_name is not null and
@@ -47,29 +46,6 @@ case extract (MONTH from started_at)
 
 -- Find out how many ride_length > 1 min and < 1 day, and create the day of week and month columns 
 select *, 
-case extract (DAYOFWEEK from started_at)
-  when 1 then 'SUN'
-  when 2 then 'MON'
-  when 3 then 'TUE'
-  when 4 then 'WED'
-  when 5 then 'THU'
-  when 6 then 'FRI'
-  when 7 then 'SAT'
-  end as day_of_week,
-case extract (MONTH from started_at)
-  when 1 then 'Jan'
-  when 2 then 'Feb'
-  when 3 then 'Mar'
-  when 4 then 'Apr'
-  when 5 then 'May'
-  when 6 then 'Jun'
-  when 7 then 'Jul'
-  when 8 then 'Aug'
-  when 9 then 'Sep'
-  when 10 then 'Oct'
-  when 11 then 'Nov'
-  when 12 then 'Dec'
-  end as Month
    from`lithe-bazaar-443112-i8.Cycalitic_Trips.clean_combined_table_2022`
   where start_station_name is not null and 
   end_station_name is not null and
