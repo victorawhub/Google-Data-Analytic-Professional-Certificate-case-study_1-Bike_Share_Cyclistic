5180c5f94592d70f080791d3dfc9b7b0fed23ee9
victoraw97 <72541996+victoraw97@users.noreply.github.com>
2025-02-19 20:58:25 +0800

    Update 03. Data cleaning.sql

--- 03. Data cleaning.sql
+++ 03. Data cleaning.sql
@@ -12,7 +12,7 @@ select *
    from`lithe-bazaar-443112-i8.Cycalitic_Trips.clean_combined_table_2022`
 
 
--- Find out how many ride_length >= 1 day, and create the day of week and month columns 
+-- Find out how many ride_length > 1 day, and create the day of week and month columns 
 select *, 
 case extract (DAYOFWEEK from started_at)
   when 1 then 'SUN'
@@ -43,3 +43,37 @@ case extract (MONTH from started_at)
   end_lat is not null and 
   ride_length >= 1440
   order by ride_length desc 
+
+
+-- Find out how many ride_length > 1 min and < 1 day, and create the day of week and month columns 
+select *, 
+case extract (DAYOFWEEK from started_at)
+  when 1 then 'SUN'
+  when 2 then 'MON'
+  when 3 then 'TUE'
+  when 4 then 'WED'
+  when 5 then 'THU'
+  when 6 then 'FRI'
+  when 7 then 'SAT'
+  end as day_of_week,
+case extract (MONTH from started_at)
+  when 1 then 'Jan'
+  when 2 then 'Feb'
+  when 3 then 'Mar'
+  when 4 then 'Apr'
+  when 5 then 'May'
+  when 6 then 'Jun'
+  when 7 then 'Jul'
+  when 8 then 'Aug'
+  when 9 then 'Sep'
+  when 10 then 'Oct'
+  when 11 then 'Nov'
+  when 12 then 'Dec'
+  end as Month
+   from`lithe-bazaar-443112-i8.Cycalitic_Trips.clean_combined_table_2022`
+  where start_station_name is not null and 
+  end_station_name is not null and
+  end_lat is not null and 
+  ride_length > 1 and ride_length < 1440
+  order by ride_length desc 
+
