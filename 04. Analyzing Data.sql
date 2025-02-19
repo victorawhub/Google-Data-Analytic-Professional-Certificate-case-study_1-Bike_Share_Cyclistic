9db563fdae935ab8d41e24961f6d4ab3922b0733
victoraw97 <72541996+victoraw97@users.noreply.github.com>
2025-02-19 22:41:41 +0800

    Update 04. Analyzing Data.sql

--- 04. Analyzing Data.sql
+++ 04. Analyzing Data.sql
@@ -5,3 +5,21 @@ from`lithe-bazaar-443112-i8.Cycalitic_Trips.clean_combined_table_2022`
 group by member_casual, rideable_type
 order by member_casual, total_trip_rideable
 
+  
+-- No of user type per month
+select member_casual, month,
+count(*) as total_trip_month
+from `lithe-bazaar-443112-i8.Cycalitic_Trips.clean_combined_table_2022`
+group by member_casual, month
+order by member_casual
+
+
+--No of user type per day
+select member_casual, day_of_week,
+count(*) as total_trip_day
+from `lithe-bazaar-443112-i8.Cycalitic_Trips.clean_combined_table_2022`
+group by member_casual, day_of_week
+order by member_casual, total_trip_day
+
+
+-- No of user type per hour of day
